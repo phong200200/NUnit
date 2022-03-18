ffa062509420ac7368719a33dfbf3158a720e797
phong200200 <quocphong20022000@gmail.com>
2022-03-18 14:51:46 +0700

    phongv2 lan thu 16

--- TestableApp/Program.cs
+++ TestableApp/Program.cs
@@ -5,6 +5,40 @@ namespace TestableApp
 {
     public class Program
     {
+       
+
+        public List<Employee> AllUsers()
+        {
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { Id = 100, Name = "Asize", Geneder = "male", salary = 40000 });
+            employees.Add(new Employee { Id = 101, Name = "Bsize",  Geneder = "Female", salary = 56000 });
+            employees.Add(new Employee { Id = 102, Name = "Vsize", Geneder = "male", salary = 44000 });
+            employees.Add(new Employee { Id = 103, Name = "Csize", Geneder = "Female", salary = 50000 });
+            employees.Add(new Employee { Id = 104, Name = "Dsize", Geneder = "male", salary = 40500 });
+            employees.Add(new Employee { Id = 105, Name = "Esize", Geneder = "Female", salary = 52000 });
+            employees.Add(new Employee { Id = 106, Name = "Fsize", Geneder = "male", salary = 40020 });
+            employees.Add(new Employee { Id = 107, Name = "Gsize", Geneder = "Female", salary = 52000 });
+
+            employees.Add(new Employee { Id = 108, Name = "Esize", Geneder = "Female", salary = 52000 });
+            employees.Add(new Employee { Id = 109, Name = "Fsize", Geneder = "male", salary = 40020 });
+            employees.Add(new Employee { Id = 110, Name = "Gsize", Geneder = "Female", salary = 52000 });
+
+            return employees;
+        }
+        public List<Employee> GetEmployees(int id)
+        {
+            List<Employee> employees = new List<Employee>();
+            Program p = new Program();
+            var list = p.AllUsers();
+            foreach (var x in list)
+            {
+                if (x.Id == id)
+                {
+                    employees.Add(x);
+                }
+            }
+            return employees;
+        }
         public string Login(string UserId, string Password)
         {
             if (string.IsNullOrEmpty(UserId) || string.IsNullOrEmpty(Password))
@@ -20,36 +54,7 @@ namespace TestableApp
                 return "Incorrect UserId or Password.";
             }
         }
-
-        public List<Employee> AllUsers()
-        {
-            List<Employee> employees = new List<Employee>();
-            employees.Add(new Employee { Id = 100, Name = "Asize", Geneder = "male", salary = 40000 });
-            employees.Add(new Employee { Id = 101, Name = "Bsize",  Geneder = "Female", salary = 56000 });
-            employees.Add(new Employee { Id = 102, Name = "Vsize", Geneder = "male", salary = 44000 });
-            employees.Add(new Employee { Id = 103, Name = "Csize", Geneder = "Female", salary = 50000 });
-            employees.Add(new Employee { Id = 104, Name = "Dsize", Geneder = "male", salary = 40500 });
-            employees.Add(new Employee { Id = 105, Name = "Esize", Geneder = "Female", salary = 52000 });
-            employees.Add(new Employee { Id = 106, Name = "Fsize", Geneder = "male", salary = 40020 });
-            employees.Add(new Employee { Id = 107, Name = "Gsize", Geneder = "Female", salary = 52000 });
-
-            return employees;
-        }
-
-        public List<Employee> GetEmployees(int id)
-        {
-            List<Employee> employees = new List<Employee>();
-            Program p = new Program();
-            var list = p.AllUsers();
-            foreach (var x in list)
-            {
-                if (x.Id == id)
-                {
-                    employees.Add(x);
-                }
-            }
-            return employees;
-        }
+        
       
 
         static void Main(string[] args)
